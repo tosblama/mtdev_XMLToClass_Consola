fb64b29bd887226c3132f31d0504bb6aeca7c6d2
ElProgramadorVago <ElProgramadorVago@W11-EPV>
2022-12-02 11:03:55 +0100

    Agregar archivos de proyecto.

---
+++ XMLToClass_Consola/Clases/xmlUtil.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLToClass_Consola.Clases
+{
+    internal class xmlUtil
+    {
+
+        public bool DesSerializar(ref Root clase, string ficheroXml)
+        {
+            try
+            {
+                var objStreamReader = new System.IO.StreamReader(ficheroXml);
+                var x = new System.Xml.Serialization.XmlSerializer(clase.GetType());
+
+                clase = (Root)x.Deserialize(objStreamReader);
+                objStreamReader.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public bool Serializar(Root clase, string ficheroXmlDestino)
+        {
+            try
+            {
+                var objStreamWriter = new System.IO.StreamWriter(ficheroXmlDestino, false, System.Text.Encoding.GetEncoding("UTF-8"));
+                var x = new System.Xml.Serialization.XmlSerializer(clase.GetType());
+
+                x.Serialize(objStreamWriter, clase);
+                objStreamWriter.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ XMLToClass_Consola/Program.cs
@@ -0,0 +1,34 @@
+﻿// Pasar fichero XML a clase, y recorrer los clientes
+using XMLToClass_Consola.Clases;
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Pulsa intro para pasar el fichero a una clase y recorrerla");
+Console.ForegroundColor = ConsoleColor.White;
+Console.ReadLine();
+
+Root r = new Root();
+string ficheroXML = @"C:\MTDEV\VisualStudio\XMLToClass_Consola\XMLToClass_Consola\xsd_schemas\CustomersOrders.xml";
+var xUtil = new xmlUtil();
+xUtil.DesSerializar(ref r, ficheroXML);
+
+foreach(var c in r.Customers)
+{
+    Console.WriteLine(c.ContactName);
+}
+
+
+// Modificar un cliente en la clase y mostrarlos de nuevo
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Pulsa intro para modificar un cliente y recorrer de nuevo la clase");
+Console.ForegroundColor = ConsoleColor.White;
+Console.ReadLine();
+
+
+r.Customers[1].ContactName = "Cliente Modificado";
+string ficheroResultado = "C:\\MTDEV\\VisualStudio\\XMLToClass_Consola\\XMLToClass_Consola\\Clases\\NuevoFichero.xml";
+xUtil.Serializar(r, ficheroResultado);
+Console.ReadLine();
+
+
+
